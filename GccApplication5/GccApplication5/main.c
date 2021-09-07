d11277ddb826dbd7fb7e84ce9913c1763b44d023
Rajat1agva <rajatmishra@agvahealthcare.com>
2021-09-07 15:58:42 +0530

    first commit

---
+++ GccApplication5/GccApplication5/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication5.c
+ *
+ * Created: 9/7/2021 3:50:02 PM
+ * Author : agva
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
